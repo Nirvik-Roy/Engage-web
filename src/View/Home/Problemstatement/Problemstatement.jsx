f4650b87a72884258b5c6398d372630bbef51dd8
Nirvik Roy <nirvikroy802@gmail.com>
2026-01-20 14:16:10 +0530

    before after slider done

--- src/View/Home/Problemstatement/Problemstatement.jsx
+++ src/View/Home/Problemstatement/Problemstatement.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import './Problemstatement.css'
-import afterImg from '../../../assets/After.png'
+import afterImg from '../../../assets/after (2).png'
+import beforeImg from '../../../assets/before.png'
+import ReactCompareImage from "react-compare-image";
+import buttonImg from '../../../assets/Button.svg'
 const Problemstatement = () => {
     return (
         <>
             <div className='problem_statement_wrapper'>
                 <div className='container problem_statement_content_wrapper'>
-                <div className='after_img_div'>
-                    <img src={afterImg}  alt='image'/>
-                </div>
+                    <div className='after_img_div'>
+                        <ReactCompareImage
+                            leftImage={beforeImg}
+                            rightImage={afterImg}
+                            handle={
+                                <img src={buttonImg} style={{
+                                    width: '70px',
+                                    height: '70px'
+                                }} />
+                            }
+                            sliderLineWidth={6}
+                        />
+                    </div>
                     <div className='statement_problem_div'>
                         <h1>Problem Statement</h1>
                         <h3>No more expensive staff activities with low participation</h3>
@@ -20,7 +33,7 @@ const Problemstatement = () => {
                             <li>No clear way to track what’s actually working</li>
 
                             <p style={{
-                                marginTop:'40px'
+                                marginTop: '40px'
                             }}>With tight budgets and year-round expectations, keeping people engaged can feel tiring.</p>
                         </ul>
                     </div>
