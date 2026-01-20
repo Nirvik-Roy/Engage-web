b2ab5edd54efe7d9a23acf25d5aa3914d0b8536e
Nirvik Roy <nirvikroy802@gmail.com>
2026-01-20 18:56:56 +0530

    drag speed not fixed

--- src/View/Home/Problemstatement/Problemstatement.jsx
+++ src/View/Home/Problemstatement/Problemstatement.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Problemstatement.css'
 import afterImg from '../../../assets/after (2).png'
 import beforeImg from '../../../assets/before.png'
 import ReactCompareImage from "react-compare-image";
 import buttonImg from '../../../assets/Button.svg'
 const Problemstatement = () => {
+    const [position, setPosition] = useState(50);
+
+    const handlePositionChange = (rawPos) => {
+        // rawPos is from 0 to 1
+        const speedFactor = 10; // >1 makes it faster, <1 slower
+        let scaledPos = rawPos * speedFactor;
+        // clamp (keep between 0 and 1)
+        scaledPos = Math.max(0, Math.min(1, scaledPos));
+        setPosition(scaledPos * 100);
+    };
     return (
         <>
             <div className='problem_statement_wrapper'>
@@ -19,6 +29,11 @@ const Problemstatement = () => {
                                     height: '70px'
                                 }} />
                             }
+                            // controlled position
+                            sliderPositionPercentage={position / 100}
+
+                            // intercept slider position
+                            onSliderPositionChange={handlePositionChange}
                             sliderLineWidth={6}
                         />
                     </div>
