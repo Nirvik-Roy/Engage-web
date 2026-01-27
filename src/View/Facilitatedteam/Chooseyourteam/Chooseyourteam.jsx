5111fdef1067c5c7f979c8e961b8ac5a15407fd9
Nirvik Roy <nirvikroy802@gmail.com>
2026-01-27 19:59:50 +0530

    facilitated team page done

--- src/View/Facilitatedteam/Chooseyourteam/Chooseyourteam.jsx
+++ src/View/Facilitatedteam/Chooseyourteam/Chooseyourteam.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import cardImg from '../../../assets/c916209092a6d3fef78320b3722db5f4b286cbe1.jpg'
 import cardImg2 from '../../../assets/76eb601ae8cbdb510a11f197e2af52a513a36fc9.jpg'
 import cardImg3 from '../../../assets/b191c8ca226e6d6e84daec673db7abc4b9a3a71b.jpg'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick'
 const Chooseyourteam = () => {
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    var settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        centerMode: true,
+        centerPadding: "10px",
+        responsive: [
+            {
+                breakpoint: 750,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    centerPadding: "20px",
+                }
+            },
+        ]
+    };
     const cardData = [
         {
             id: 1,
@@ -88,12 +123,33 @@ const Chooseyourteam = () => {
 
                 ))}
             </div>
-            <span style={{
+
+            <div className='engage_rythm_cards_wrapper responsive_rythm_cards_wrapper'>
+                {mounted && <Slider {...settings}>
+
+                    {cardData.map((e, i) => (
+                        <div key={i} className='engage_rythm_card' >
+                            <div className='engage_rythm_img'>
+                                <img src={e.img} />
+                                <div className='number'>{e.id}</div>
+                            </div>
+                            <h4>{e.title}</h4>
+                            <small>{e.para}</small>
+                        </div>
+
+                    ))}
+                </Slider>}
+            </div>
+            <span className='container' style={{
                 fontWeight: '700',
                 textAlign: 'center',
                 display: 'block',
                 marginTop: '20px'
             }}>Pricing varies by experience and format. Select an experience to receive a tailored quote.</span>
+
+
+
+
         </>
     )
 }
