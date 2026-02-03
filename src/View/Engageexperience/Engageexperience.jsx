dff81fa223f52885b0c4d584db0dadcbc61d200b
Nirvik Roy <nirvikroy802@gmail.com>
2026-02-03 17:39:25 +0530

    url changed

--- src/View/Engageexperience/Engageexperience.jsx
+++ src/View/Engageexperience/Engageexperience.jsx
@@ -236,7 +236,7 @@ const Engageexperience = () => {
                                         color: 'rgba(31, 144, 31, 1)',
                                         cursor: 'pointer'
                                     }} onClick={() => {
-                                        const url = `http://localhost:5173/checkout?category=${encodeURIComponent(category)}&subcategory=${encodeURIComponent(filter)}&price=${encodeURIComponent(price)}&totalprice=${encodeURIComponent(totalprice)}&experience=${encodeURIComponent(element.title)}`;
+                                        const url = `https://engage-web-eight.vercel.app/checkout?category=${encodeURIComponent(category)}&subcategory=${encodeURIComponent(filter)}&price=${encodeURIComponent(price)}&totalprice=${encodeURIComponent(totalprice)}&experience=${encodeURIComponent(element.title)}`;
                                         window.open(url, "_blank"); // "_blank" opens in new tab
                                     }}>Procced</p> : <p style={{
                                         display: 'flex',
